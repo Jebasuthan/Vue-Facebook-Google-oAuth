434a96e7f0d3f719a8547f7aa4f58589b0381606
Jeba Suthan <itjebasuthan@gmail.com>
2019-10-03 13:05:33 +0530

    Signup with Facebook and Google sample application implementation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/router'
+import store from './store/store'
 
+import '@/assets/css/style.css'
+
+import GoogleAuth from '@/config/google.js'
+const gauthOption = {
+  clientId: '707231563844-e5cpkqrlt62gncmj6b84of5sml9lp8g9.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+Vue.use(GoogleAuth, gauthOption)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HomeMenu />
+    <div class="main-container">
+      <div class="loginsuccess-container">
+        <h2 class="heading">Login Success!</h2>
+        <div>
+          <div v-if="this.$store.state.loginUser.loginType == 'fb'">
+            Facebook Access Token - {{this.$store.state.loginUser.fb.authResponse.accessToken}}
+          </div>
+          <div v-else-if="this.$store.state.loginUser.loginType == 'google'">
+            Google Access Token - {{this.$store.state.loginUser.google.Zi.access_token}}
+            <p>Welcome {{this.$store.state.loginUser.google.w3.ig}} !</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeMenu from '@/components/Menu'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HomeMenu
   }
 }
 </script>

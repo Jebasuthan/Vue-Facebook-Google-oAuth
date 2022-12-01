0433de43d331b8375a628e1733392ce2500381dd
Jeba Suthan <itjebasuthan@gmail.com>
2022-12-01 11:58:54 -0600

    Git pages deployment added

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com/Jebasuthan/Vue-Facebook-Google-oAuth.git master:gh-pages
+cd -
